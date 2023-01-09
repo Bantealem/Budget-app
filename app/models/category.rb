6517ef7bbf1268c5e10690a3d93e22586684d744
bantealem <bantem2021@gmail.com>
2023-01-09 17:36:37 +0300

    Add Validations to Models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :category_transactions
     has_many :be_transactions, through: :category_transactions
+    validates :name, presence: true
+    validates :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :be_transactions
     has_many :Category
+    validates :name, presence: true
 end
